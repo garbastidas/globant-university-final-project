78b71d18b141a9bac7a92bf33c1cf90bb0b8e621
garoperob@unal.edu.co <garbastidas@gmail.com>
2025-05-19 23:39:17 -0500

    methods no usages

--- src/com/globant/java/models/University.java
+++ src/com/globant/java/models/University.java
@@ -10,6 +10,42 @@ public class University {
     private List<Teacher> teachers = new ArrayList<>();
     private List<Student> students = new ArrayList<>();
 
+    Scanner read = new Scanner(System.in);
+
+    public University() {
+        // Professors
+        Teacher teacher1 = new FullTimeTeacher("Fernando", 3, true);
+        Teacher teacher2 = new PartTimeTeacher("Hernando", 20, false);
+        List<Teacher> teachers_s = new ArrayList<>();
+        teachers_s.add(teacher1); teachers_s.add(teacher2);
+        // Students
+        Student student1 = new Student("Javier", 22);
+        Student student2 = new Student("María", 21);
+        Student student3 = new Student("Juan", 22);
+        Student student4 = new Student("Jazmín", 23);
+        Student student5 = new Student("Ricardo", 22);
+        Student student6 = new Student("Azucena", 20);
+        List<Student> students_s = new ArrayList<>();
+        students_s.add(student1);students_s.add(student2);students_s.add(student3);
+        students_s.add(student4);students_s.add(student5);students_s.add(student6);
+        // Classes
+        Class class1 = new Class("Biology", teacher1, 101);
+        Class class2 = new Class("Biochemistry", teacher1, 102);
+        Class class3 = new Class("Calculus I", teacher2, 103);
+        Class class4 = new Class("Calculus II", teacher2, 104);
+        for(Student student: students_s){
+            class1.addStudent(student);
+            class2.addStudent(student);
+            class3.addStudent(student);
+            class4.addStudent(student);
+        }
+        List<Class> classes_s = new ArrayList<>();
+        classes_s.add(class1); classes_s.add(class2); classes_s.add(class3); classes_s.add(class4);
+        setStudents(students_s);
+        setTeachers(teachers_s);
+        setClasses(classes_s);
+    }
+
     public void setClasses(List<Class> classes) {
         this.classes = classes;
     }
@@ -22,26 +58,17 @@ public class University {
         this.students = students;
     }
 
-    Scanner read = new Scanner(System.in);
     public void printStudents(){
         for (int i = 0; i < students.size() ; i++){
             System.out.println(i + "\t" + students.get(i).getName());
         }
     }
-    public void addTeacher(String name, boolean isFullTime){
-        if (isFullTime){
-            System.out.println("Enter the years of experience: ");
-            Teacher teacher = new FullTimeTeacher(name, read.nextInt(), isFullTime);
-        } else {
-            System.out.println("Enter active hours: ");
-            Teacher teacher = new PartTimeTeacher(name, read.nextInt(), isFullTime);
-        }
-    }
+
     public void printTeachers(){
         for (int i = 0; i < teachers.size() ; i++){
             String typeTeacher = teachers.get(i).isFullTime() ? "Full time": "Part time";
             System.out.println(i + "\t" + teachers.get(i).getName() +
-                    "\t" + teachers.get(i).getBaseSalary() + typeTeacher);
+                    "\t" + teachers.get(i).getBaseSalary() + " " + typeTeacher);
         }
     }
     private void submenuClass(int classAsk){
--- src/com/globant/java/principal/Principal.java
+++ src/com/globant/java/principal/Principal.java
@@ -12,37 +12,6 @@ public class Principal {
         Scanner read = new Scanner(System.in);
         // University
         University university = new University();
-        // Professors
-        Teacher teacher1 = new FullTimeTeacher("Fernando", 3, true);
-        Teacher teacher2 = new PartTimeTeacher("Hernando", 20, false);
-        List<Teacher> teachers_s = new ArrayList<>();
-        teachers_s.add(teacher1); teachers_s.add(teacher2);
-        // Students
-        Student student1 = new Student("Javier", 22);
-        Student student2 = new Student("María", 21);
-        Student student3 = new Student("Juan", 22);
-        Student student4 = new Student("Jazmín", 23);
-        Student student5 = new Student("Ricardo", 22);
-        Student student6 = new Student("Azucena", 20);
-        List<Student> students_s = new ArrayList<>();
-        students_s.add(student1);students_s.add(student2);students_s.add(student3);
-        students_s.add(student4);students_s.add(student5);students_s.add(student6);
-        // Classes
-        Class class1 = new Class("Biology", teacher1, 101);
-        Class class2 = new Class("Biochemistry", teacher1, 102);
-        Class class3 = new Class("Calculus I", teacher2, 103);
-        Class class4 = new Class("Calculus II", teacher2, 104);
-        for(Student student: students_s){
-            class1.addStudent(student);
-            class2.addStudent(student);
-            class3.addStudent(student);
-            class4.addStudent(student);
-        }
-        List<Class> classes_s = new ArrayList<>();
-        classes_s.add(class1); classes_s.add(class2); classes_s.add(class3); classes_s.add(class4);
-        university.setStudents(students_s);
-        university.setTeachers(teachers_s);
-        university.setClasses(classes_s);
         //Begining Menu
         System.out.println("a. Professors");
         System.out.println("b. Classes");
